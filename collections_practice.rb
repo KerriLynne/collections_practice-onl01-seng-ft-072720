8159d0d5aa41392bc3cb55bab0900c2823c209eb
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-07-31 16:18:00 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -5,5 +5,11 @@ end
 def sort_array_desc(num)
  num.sort do |a, b|
    b <=> a
- end   
+ end
+end
+
+def sort_array_char_count(strings)
+  strings.sort do |a, b|
+    a.length <=> b.length
+  end
 end
